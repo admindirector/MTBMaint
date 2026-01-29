f580b55e827565f9e75c3fd45c7c9e8e5faf4061
admindirector <admindirector@users.noreply.github.com>
2026-01-29 12:10:23 -0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
+base: '/MTBMaint/',  
+plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
